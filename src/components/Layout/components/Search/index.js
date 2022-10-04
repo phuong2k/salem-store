b2ff226ccbe2b3932042c71298fb7e0b981bb25a
phuong <huynhphuong.fsd@gmail.com>
2022-10-04 18:16:55 +0700

    fix

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -10,66 +10,67 @@ import { db } from '../../../../firebase/config';
 import { Select, Spin, Avatar, Input } from 'antd';
 
 const cx = classNames.bind(styles);
-function DebounceSelect({ fetchOptions, debounceTimeout = 300, curMembers, ...props }) {
-    const [fetching, setFetching] = useState(false);
-    const [options, setOptions] = useState([]);
-    const debounceFetcher = React.useMemo(() => {
-        const loadOptions = (value) => {
-            setOptions([]);
-            setFetching(true);
+// function DebounceSelect({ fetchOptions, debounceTimeout = 300, curMembers, ...props }) {
+//     // const [fetching, setFetching] = useState(false);
+//     // const [options, setOptions] = useState([]);
+//     // const debounceFetcher = React.useMemo(() => {
+//     //     const loadOptions = (value) => {
+//     //         setOptions([]);
+//     //         setFetching(true);
 
-            fetchOptions(value, curMembers).then((newOptions) => {
-                setOptions(newOptions);
-                setFetching(false);
-            });
-        };
+//     //         fetchOptions(value, curMembers).then((newOptions) => {
+//     //             setOptions(newOptions);
+//     //             setFetching(false);
+//     //         });
+//     //     };
 
-        return debounce(loadOptions, debounceTimeout);
-    }, [debounceTimeout, fetchOptions, curMembers]);
+//     //     return debounce(loadOptions, debounceTimeout);
+//     // }, [debounceTimeout, fetchOptions, curMembers]);
 
-    React.useEffect(() => {
-        return () => {
-            // clear when unmount
-            setOptions([]);
-        };
-    }, []);
+//     // React.useEffect(() => {
+//     //     return () => {
+//     //         // clear when unmount
+//     //         setOptions([]);
+//     //     };
+//     // }, []);
 
-    return (
-        <Select
-            // labelInValue
-            // filterOption={false}
-            notFoundContent={fetching ? <Spin size="small" /> : null}
-            {...props}
-        >
-            {options.map((opt) => (
-                <Select.Option key={opt.value} value={opt.value} title={opt.label}>
-                    <Avatar size="small" src={opt.photoURL}>
-                        {opt.photoURL ? '' : opt.label?.charAt(0)?.toUpperCase()}
-                    </Avatar>
-                    {` ${opt.label}`}
-                </Select.Option>
-            ))}
-        </Select>
-    );
-}
+//     return (
+//         <div></div>
+//         // <Select
+//         //     // labelInValue
+//         //     // filterOption={false}
+//         //     notFoundContent={fetching ? <Spin size="small" /> : null}
+//         //     {...props}
+//         // >
+//         //     {options.map((opt) => (
+//         //         <Select.Option key={opt.value} value={opt.value} title={opt.label}>
+//         //             <Avatar size="small" src={opt.photoURL}>
+//         //                 {opt.photoURL ? '' : opt.label?.charAt(0)?.toUpperCase()}
+//         //             </Avatar>
+//         //             {` ${opt.label}`}
+//         //         </Select.Option>
+//         //     ))}
+//         // </Select>
+//     );
+// }
 
-async function fetchUserList(search, curProduct) {
-    return db
-        .collection('users')
-        .where('keywords', 'array-contains', search?.toLowerCase())
-        .orderBy('displayName')
-        .limit(20)
-        .get()
-        .then((snapshot) => {
-            return snapshot.docs
-                .map((doc) => ({
-                    label: doc.data().title,
-                    value: doc.data().id,
-                    photoURL: doc.data().image01,
-                }))
-                .filter((opt) => !curProduct.includes(opt.value));
-        });
-}
+// async function fetchUserList(search, curProduct) {
+//     return db
+//         .collection('users')
+//         .where('keywords', 'array-contains', search?.toLowerCase())
+//         .orderBy('displayName')
+//         .limit(20)
+//         .get()
+//         .then((snapshot) => {
+//             return snapshot.docs
+//                 .map((doc) => ({
+//                     label: doc.data().title,
+//                     value: doc.data().id,
+//                     photoURL: doc.data().image01,
+//                 }))
+//                 .filter((opt) => !curProduct.includes(opt.value));
+//         });
+// }
 export default function Search() {
     const { headerLanguage } = React.useContext(AppContext);
     const [value, setValue] = useState('');
@@ -89,18 +90,6 @@ export default function Search() {
                         type="text"
                         placeholder={headerLanguage[0].searchInput}
                         onChange={handleInputChange}
-                        onSearch={debounceFetcher}
-                    />
-
-                    <DebounceSelect
-                        mode="multiple"
-                        name="search-user"
-                        label="Tên các thành viên"
-                        value={value}
-                        placeholder="Nhập tên thành viên"
-                        fetchOptions={fetchUserList}
-                        onChange={(newValue) => setValue(newValue)}
-                        style={{ width: '100%' }}
                     />
 
                     <div className={cx('delete')}>
